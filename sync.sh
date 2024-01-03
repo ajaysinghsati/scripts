18b248168930fe544fc97fefd3606ec69d56b9ab
ajaysinghsati <125855251+ajaysinghsati@users.noreply.github.com>
2024-01-03 08:03:57 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -2,7 +2,7 @@
 
 echo
 echo "--------------------------------------"
-echo "    derpfest14 14.0 Buildbot          "
+echo "    RisingOSSTech 14 14.0 Buildbot    "
 echo "                  by                  "
 echo "            ajaysinghsati             "
 echo "--------------------------------------"
@@ -10,10 +10,17 @@ echo
 #Remove local manifest
 rm -rf .repo/local_manifests
 #Clone trees local manifest
-git clone https://github.com/ajaysinghsati/local_manifest.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/ajaysinghsati/local_manifest.git --depth 1 -b risingos-14 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 #remove rm -rf hardware/google/pixel/kernel_headers
 rm -rf hardware/google/pixel/kernel_headers
 # Run build script 
 chmod u+x scripts/build.sh
 ./scripts/build.sh
+echo
+echo "--------------------------------------"
+echo "    Run Build script Buildbot         "
+echo "              by                      "
+echo "         ajaysinghsati                "
+echo "--------------------------------------"
+echo
