12f578ed908089ef764477e3f43a6fe5612d6d82
ajaysinghsati <125855251+ajaysinghsati@users.noreply.github.com>
2024-01-04 19:56:32 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -9,13 +9,16 @@ echo "--------------------------------------"
 echo
 #Remove local manifest
 rm -rf .repo/local_manifests
+rm -rf hardware/oplus
+rm -rf device/oneplus/oscar
+rm -rf device/oneplus/sm6375-common
+
 #Clone trees local manifest
 git clone https://github.com/ajaysinghsati/local_manifest.git --depth 1 -b risingos-14 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 #remove rm -rf hardware/google/pixel/kernel_headers
 rm -rf hardware/google/pixel/kernel_headers
 rm -rf vendor/rising
-git clone https://github.com/ajaysinghsati/hardware_oplus.git -b risingos-14
 # Run build script 
 chmod u+x scripts/build.sh
 ./scripts/build.sh
