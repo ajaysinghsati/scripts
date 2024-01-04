9df16b801bf733b30694826d9103afafbda0fdf8
ajaysinghsati <125855251+ajaysinghsati@users.noreply.github.com>
2024-01-04 20:12:26 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -12,13 +12,13 @@ rm -rf .repo/local_manifests
 rm -rf hardware/oplus
 rm -rf device/oneplus/oscar
 rm -rf device/oneplus/sm6375-common
-
+rm -rf vendor/rising
+git clone https://github.com/ajaysinghsati/android_vendor_rising.git vendor/rising
 #Clone trees local manifest
 git clone https://github.com/ajaysinghsati/local_manifest.git --depth 1 -b risingos-14 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 #remove rm -rf hardware/google/pixel/kernel_headers
 rm -rf hardware/google/pixel/kernel_headers
-rm -rf vendor/rising
 # Run build script 
 chmod u+x scripts/build.sh
 ./scripts/build.sh
