21749d76c87cd6bb6d8d6e24aa64cb22120284a2
ajaysinghsati <125855251+ajaysinghsati@users.noreply.github.com>
2024-01-05 09:47:30 +0530

    Update rising14.sh

--- rising14.sh
+++ rising14.sh
@@ -4,5 +4,3 @@ rm -rf .repo
 rm -rf *
 repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
 repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j$(nproc --all)
-chmod u+x scripts/sync.sh
-./scripts/sync.sh
