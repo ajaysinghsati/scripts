573800792ec675a00a28658b0bbac12312829589
ajaysinghsati <125855251+ajaysinghsati@users.noreply.github.com>
2024-01-08 04:33:00 +0530

    Update derpfest14.sh

--- derpfest14.sh
+++ derpfest14.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 echo
 echo "--------------------------------------"
-echo "    derpfest14 14.0 Buildbot          "
+echo "    Project elixir 14 14.0 Buildbot          "
 echo "                  by                  "
 echo "            ajaysinghsati             "
 echo "--------------------------------------"
 echo
 rm -rf .repo
 rm -rf *
-repo init -u https://github.com/DerpFest-AOSP/manifest.git -b 14 --git-lfs
-repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
+repo init --depth=1 -u https://github.com/Project-Elixir/manifest -b UNO
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 #Remove local manifest
 rm -rf .repo/local_manifests
 #Clone trees local manifest
-git clone https://github.com/ajaysinghsati/local_manifest.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/ajaysinghsati/local_manifest.git --depth 1 -b elixer .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 #remove rm -rf hardware/google/pixel/kernel_headers
 rm -rf hardware/google/pixel/kernel_headers
@@ -25,21 +25,4 @@ lunch derp_oscar-user
 export TZ=Asia/Dhaka
 #make command
 mka derp
-# Check build status and display animation
-if [ $? -eq 0 ]; then
-    echo "Build successful!"
-    printf "\e[?25l"  # Hide cursor
-    cat <<EOF
-        _____
-      .'* *.'
-     / _  _ \
-    |(o|o)| |
-      \___/
-   Build by Ajay
-       with ⚡
- EOF
-    figlet -f slant "Lightning Dart"
-    printf "\e[?25h"  # Show cursor
-else
-    echo "Build failed. Please check the logs for errors."
-fi
+
